a924084bffbf2bb63e152691bc6fa23db531006c
Daghan Turkdogan <daghanturkdogan@Daghans-MacBook-Pro.local>
2025-02-12 11:47:58 +0000

    v1.1

--- kanban-board/vite.config.js
+++ kanban-board/vite.config.js
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
